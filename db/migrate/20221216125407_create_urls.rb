0c37f95258731765a919522d83ca4301a56fec8f
Carlos Del Real <carloshdelreal@gmail.com>
2022-12-16 09:59:17 -0500

    feat: validate uniqueness of url, get rid of count

--- db/migrate/20221216125407_create_urls.rb
+++ db/migrate/20221216125407_create_urls.rb
@@ -2,10 +2,8 @@ class CreateUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :urls do |t|
       t.string :title
-      t.string :url
+      t.string :url, unique: true
       t.string :source
-      t.integer :counter, default: 0
-
       t.timestamps
     end
   end
