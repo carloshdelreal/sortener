019a6c94613ffdaf7f7ed43e7b799ee97d09a28d
Carlos Del Real <carloshdelreal@gmail.com>
2022-12-16 14:00:40 -0500

    test: update failing tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -10,13 +10,6 @@ RSpec.describe Url, type: :model do
   end
 
   context "fails to create url" do
-    it "fails to create url without url" do
-      url = FactoryBot.build(:url)
-
-      url.url = nil
-      
-      expect(url).not_to be_valid
-    end
     it "fails to create url without source" do
       url = FactoryBot.build(:url)
 
