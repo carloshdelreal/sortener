3261689f60f5a66fa38f62742e38735dd1a9cdf4
Carlos Del Real <carloshdelreal@gmail.com>
2022-12-16 11:34:23 -0500

    feat: handle when url does not exist

--- app/controllers/sortener_controller.rb
+++ app/controllers/sortener_controller.rb
@@ -1,9 +1,9 @@
 class SortenerController < ApplicationController
   def sortener
     @url = Url.find_by(url: params[:sorted_url])
-    @url.visits.push(Visit.new)
-
+    
     if @url
+      @url.visits.push(Visit.new)
       redirect_to @url.source
     else
       render :text => "404 Not found", :status => 404
