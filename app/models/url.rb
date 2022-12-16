f3f6a1f78c28802b4dc20a2ecd5223872bf54b2b
Carlos Del Real <carloshdelreal@gmail.com>
2022-12-16 09:08:05 -0500

    feat: generate url before validation Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,9 @@
 class Url < ApplicationRecord
   validates_presence_of :url, :source
+
+  before_validation :generate_url
+
+  def generate_url
+    self.url = SecureRandom.uuid.slice(0,8)
+  end
 end
